6144bc3b1b35e7ba5b96b7da596b22725f8de4dd
Mastanbi551 <75158589+mastanbi551@users.noreply.github.com>
2025-06-24 17:05:57 +0530

    Lists & keys

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,10 +12,28 @@ export default function Header(props) {
     props.setIsAuth(false);
   }
 
+  const person = [
+    {
+      name: "Mastanbi",
+      age: 29,
+    },
+    {
+      name: "test",
+      age: 60,
+    },
+  ];
+
   console.log(user);
   return (
     <>
       <div className="header">Welcome {user}</div>
+      <div>
+        {person.map((p) => (
+          <li key={p.age}>
+            {p.name} - {p.age}
+          </li>
+        ))}
+      </div>
       <button type="button" onClick={(e) => onLogOut(e)}>
         LogOut
       </button>
